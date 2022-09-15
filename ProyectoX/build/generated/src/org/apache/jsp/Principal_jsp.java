ee10cda7e8b70214a67624cfce2985e758e18f41
aescobar2017379 <109198593+aescobar2017379@users.noreply.github.com>
2022-09-15 01:38:18 -0600

    Add files via upload
    Se agrego Pagina Principal 1.5

---
+++ ProyectoX/build/generated/src/org/apache/jsp/Principal_jsp.java
@@ -0,0 +1,99 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class Principal_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">           \n");
+      out.write("            <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU\" crossorigin=\"anonymous\">\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ\" crossorigin=\"anonymous\"></script>\n");
+      out.write("        <title>Proyecto X</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body id=\"Body\" style=\"background-color: green;\">\n");
+      out.write("        <nav class=\"navbar navbar-expand-lg navbar-light\" id=\"NavL\">\n");
+      out.write("            <div class=\"container-fluid \" id=\"Container\">\n");
+      out.write("                <a class=\"navbar-brand\" href=\"#\">  \n");
+      out.write("                </a>\n");
+      out.write("                <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n");
+      out.write("                    <span class=\"navbar-toggler-icon\"></span>\n");
+      out.write("                </button>\n");
+      out.write("                <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n");
+      out.write("                    <ul class=\"nav nav-tabs\" id=\"Btns\">\n");
+      out.write("                        <li class=\"nav-item\">\n");
+      out.write("                            <a class=\"nav-link text-light\" href=\"Controlador?menu=PaginaPrincipal\" target=\"myFrame\">About</a>\n");
+      out.write("                        </li>\n");
+      out.write("                        <li class=\"nav-item\">\n");
+      out.write("                            <a class=\"nav-link text-light\" href=\"Controlador?menu=Cliente&accion=Listar\" target=\"myFrame\">Articulo</a>\n");
+      out.write("                        </li>\n");
+      out.write("                        <li class=\"nav-item\">\n");
+      out.write("                            <a class=\"nav-link text-light\" href=\"Controlador?menu=Empleado&accion=Listar\" target=\"myFrame\">Carrito de Compras</a>\n");
+      out.write("                        </li>\n");
+      out.write("                                           \n");
+      out.write("                    </ul>\n");
+      out.write("                </div>\n");
+      out.write("                \n");
+      out.write("            </div>\n");
+      out.write("          </nav>\n");
+      out.write("        <div class=\"m-4\"  id=\"Frame\">\n");
+      out.write("           <iframe src=\"\" name=\"myFrame\" style=\" height: 100%; width: 100%;\" id=\"myFrame\"></iframe>\n");
+      out.write("      </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+      out.write("\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
