d3117c871944c3d430074ac011dd20c82c2541d9
Edson Patzan <epatzan-2021484@kinal.edu.gt>
2022-09-15 23:25:12 -0600

    Se agrego archivo reparado

---
+++ ProyectoX/src/java/modelo/Usuario.java
@@ -0,0 +1,53 @@
+package modelo;
+
+
+public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String email;
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nombre, String email, String clave) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.clave = clave;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
